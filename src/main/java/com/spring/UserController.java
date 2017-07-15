7824f36d075175eff4aabf1b8cf511c73042db88
Pradeep Arya <impradeeparya@gmail.com>
2017-07-15 19:12:41 +0530

    load database on the basis of environment

--- src/main/java/com/spring/UserController.java
+++ src/main/java/com/spring/UserController.java
@@ -6,10 +6,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 /**
  * Created by pradeep on 15/7/17.
  */
