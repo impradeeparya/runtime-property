8226802309a9a0271817cf229cd5528e9a45ac4c
Pradeep Arya <impradeeparya@gmail.com>
2017-07-15 18:47:39 +0530

    profile changes

---
+++ src/main/java/com/spring/UserController.java
@@ -0,0 +1,32 @@
+package com.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by pradeep on 15/7/17.
+ */
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @RequestMapping("/users")
+    public String fetchUsers() {
+
+        String query = "SELECT * FROM user where id = 1";
+
+        User user = (User) jdbcTemplate.queryForObject(query, new BeanPropertyRowMapper<User>(User.class));
+
+        return user.getName();
+    }
+}
